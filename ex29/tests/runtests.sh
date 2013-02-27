5e1c769615fecbc79309d9dcf116c17d895f058e
Hunter Bridges <hbridges@gmail.com>
2013-02-26 21:03:41 -0800

    ex29

--- ex29/tests/runtests.sh
+++ ex29/tests/runtests.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
+set -e
 echo "Running unit tests:"
 
 for i in tests/*_tests
 do
     if test -f $i
     then
-        if $VALGRIND ./$i 2>> tests/tests.log
+        if $VALGRIND ./$i build/libex29.so print_a_message "hello there" 2>> tests/tests.log
         then
             echo $i PASS
         else
